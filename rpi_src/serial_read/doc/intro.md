71e55c1fd21521a97d1ad5963a6f47c77b9febc5
Dmitrii Galantsev <dgalants@iu.edu>
2020-02-15 17:41:39 -0500

    remove clojure for serial_read

--- rpi_src/serial_read/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to serial_read
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- rpi_src/serial_read/test/serial_read/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns serial-read.core-test
-  (:require [clojure.test :refer :all]
-            [serial-read.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
